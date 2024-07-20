18384775775c5f8a8e2d283bfff45a21339d146d
Ash Dey <ashabc [at] hotmail.com>
2024-07-21 09:05:24 +1000

    added a few env variable

--- main.tf
+++ main.tf
@@ -60,6 +60,8 @@ resource "azurerm_linux_function_app" "fa" {
     comm_service_conn_string = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.comm_service_conn_string.versionless_id})"
     kv_url                   = "https://${var.env}-${var.region}-${var.company}-kv-${var.kvname}.vault.azure.net"
     cert_name                = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.cert_name.versionless_id})"
+    api_version              = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.api_version.versionless_id})"
+    vendor_subscriptions     = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.vendor_subscriptions.versionless_id})"
     # WEBSITE_TIME_ZONE      = "AU"
     WEBSITE_RUN_FROM_PACKAGE            = 1
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = true
